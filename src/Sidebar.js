6894b2abf7cd0ff03cbead9a1704b965abff94ea
Dane Iliff <wdaneiliff@gmail.com>
2017-12-23 15:36:27 -0800

    Added Velocity.js

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -11,6 +11,10 @@ return (
           <h3>Top Tracks</h3>
           <p>Dive a bit deeper. Explore top tracks we've featured in the past.</p>
         </div>
+        <div className="newsletterContainer">
+          <h3>Heads up</h3>
+          <p>Expand your mind with new music and concert updates around the area.</p>
+        </div>
       </div>
     )
   }
