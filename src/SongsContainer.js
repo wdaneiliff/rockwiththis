6ae345ed221d72373d90f2ffc9432d45f6b9b481
Dane Iliff <wdaneiliff@gmail.com>
2017-12-28 21:13:41 -0800

    Added Yarn - Redux

--- src/SongsContainer.js
+++ src/SongsContainer.js
@@ -10,32 +10,6 @@ class SongsContainer extends Component {
     }
 
 
-<<<<<<< HEAD
-render() {
-    let songs = this.props.songs.map((song, index) => {
-      return (
-        <div className="songContainer" key={index}>
-          <div className="imageContainer">
-            <img className="songImage" src={song._embedded['wp:featuredmedia'][0].media_details.sizes.full.source_url} />
-          </div>
-          <div className="postContent" >
-            <div className="topContentContainer">
-              <div className="songInfo">
-                <p className="postTitle" dangerouslySetInnerHTML={ {__html: song.title.rendered} }/>
-                  <div className="metaInfo">
-                    <span className="postAuthor">By {song._embedded.author[0].name} | </span>
-                    <span className="postDate">{song.date} | </span>
-                    <span className="postTags">{song.tags}</span>
-                  </div>
-              </div>
-            </div>
-            <div className="singlePostPlayer">
-              <button className="singlePostPlayerButton"/>
-              <div className="singlePostPlayerInfo">
-                <p className="songName"><strong>Song Name</strong>{song.acf.song_name}</p>
-                <p className="artistName"> by <strong>Artist Name</strong>{song.acf.artist_name}</p>
-              </div>
-=======
     render() {
         const posts = this.props.posts.map((song, index) => (
             <div className="songContainer" key={index}>
@@ -61,7 +35,6 @@ render() {
                     </div>
                     <p onClick={this.toggleDescription} className="expand">More<br /> <Icon name="angle-down" /></p>
                 </div>
->>>>>>> 445f040fea978925ca89787225560b267755c178
             </div>
         ))
         return (
