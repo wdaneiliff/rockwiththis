4e8b4474d8f4b3f087238c4f4424780f702b7aa9
Dane Iliff <dane@MacBook-Pro-2.local>
2018-04-26 14:39:44 -0700

    Social Links Styling + etc

--- src/SocialLinks.js
+++ src/SocialLinks.js
@@ -6,7 +6,11 @@ class SocialLinks extends Component {
         return (
           <div className="socialLinks">
             <ul>
-            <li><a target="_blank" href="https://www.facebook.com/rockwiththis/"></a></li>
+            <li><a target="_blank" href="https://www.facebook.com/rockwiththis/"><img src="http://www.rockwiththis.com/wp-content/uploads/2018/04/iconmonstr-facebook-1-48.png" /></a></li>
+            <li><a target="_blank" href="https://www.facebook.com/rockwiththis/"><img src="http://www.rockwiththis.com/wp-content/uploads/2018/04/iconmonstr-instagram-11-48.png" /></a></li>
+            <li><a target="_blank" href="https://www.facebook.com/rockwiththis/"><img src="http://www.rockwiththis.com/wp-content/uploads/2018/04/iconmonstr-twitter-1-48.png" /></a></li>
+            <li><a target="_blank" href="https://www.facebook.com/rockwiththis/"><img src="http://www.rockwiththis.com/wp-content/uploads/2018/04/iconmonstr-soundcloud-1-48.png" /></a></li>
+            <li><a target="_blank" href="https://www.facebook.com/rockwiththis/"><img src="http://www.rockwiththis.com/wp-content/uploads/2018/04/iconmonstr-spotify-1-48.png" /></a></li>
             </ul>
           </div>
         )
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import 'semantic-ui-css/semantic.min.css';
 import './stylesheets/index.css'
+
 
 ReactDOM.render((
   <BrowserRouter>
