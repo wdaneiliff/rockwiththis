d939c5ef53c907fb403dab7bad57011e7da027cd
Dane Iliff <dane@MacBook-Pro-2.local>
2018-04-26 14:51:31 -0700

    more

--- src/SocialLinks.js
+++ src/SocialLinks.js
@@ -7,10 +7,10 @@ class SocialLinks extends Component {
           <div className="socialLinks">
             <ul>
             <li><a target="_blank" href="https://www.facebook.com/rockwiththis/"><img src="http://www.rockwiththis.com/wp-content/uploads/2018/04/iconmonstr-facebook-1-48.png" /></a></li>
-            <li><a target="_blank" href="https://www.facebook.com/rockwiththis/"><img src="http://www.rockwiththis.com/wp-content/uploads/2018/04/iconmonstr-instagram-11-48.png" /></a></li>
-            <li><a target="_blank" href="https://www.facebook.com/rockwiththis/"><img src="http://www.rockwiththis.com/wp-content/uploads/2018/04/iconmonstr-twitter-1-48.png" /></a></li>
-            <li><a target="_blank" href="https://www.facebook.com/rockwiththis/"><img src="http://www.rockwiththis.com/wp-content/uploads/2018/04/iconmonstr-soundcloud-1-48.png" /></a></li>
-            <li><a target="_blank" href="https://www.facebook.com/rockwiththis/"><img src="http://www.rockwiththis.com/wp-content/uploads/2018/04/iconmonstr-spotify-1-48.png" /></a></li>
+            <li><a target="_blank" href="https://www.instagram.com/rockwiththismusic/"><img src="http://www.rockwiththis.com/wp-content/uploads/2018/04/iconmonstr-instagram-11-48.png" /></a></li>
+            <li><a target="_blank" href="https://www.twitter.com"><img src="http://www.rockwiththis.com/wp-content/uploads/2018/04/iconmonstr-twitter-1-48.png" /></a></li>
+            <li><a target="_blank" href="https://soundcloud.com/rockwiththis/sets/rock-with-this"><img src="http://www.rockwiththis.com/wp-content/uploads/2018/04/iconmonstr-soundcloud-1-48.png" /></a></li>
+            <li><a target="_blank" href="https://open.spotify.com/user/jaredp21/playlist/2eWK5PGSTEl8I5ZvMG5VPS"><img src="http://www.rockwiththis.com/wp-content/uploads/2018/04/iconmonstr-spotify-1-48.png" /></a></li>
             </ul>
           </div>
         )
