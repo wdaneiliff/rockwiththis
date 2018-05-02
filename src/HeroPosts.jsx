0ad80ae0b376eb289fb4e231544067974de35035
Alex Glover <alex@aglover.local>
2018-05-01 23:28:11 -0400

    iterate through posts, responsive hero grid

--- src/HeroPosts.jsx
+++ src/HeroPosts.jsx
@@ -19,60 +19,51 @@ class HeroPosts extends React.Component {
                 artist_name: 'React'
             }
         }
-        const { posts } = this.props
-        const featuredPost = posts[0] || placeholderSong
-        const featuredImage = featuredPost.better_featured_image.source_url
-        const featuredMonth = moment(featuredPost.date).format('MMM')
-        const featuredDate = moment(featuredPost.date).format('D')
-        const featuredTitle = featuredPost.acf.song_name
-        const featuredArtist = featuredPost.acf.artist_name
-        const otherPosts = this.props.posts.slice(6)
 
-        const previousWeekPosts = otherPosts.map((post) => {
-          <div className='feature-post small'>
-            <img src="https://www.rockwiththis.com/wp-admin/upload.php?item=12679" />
-          </div>
-        })
+        const trackDisplay = (post, isSmall) => {
+            const image = post.better_featured_image.source_url
+            const month = moment(post.date).format('MMM')
+            const date = moment(post.date).format('D')
+            const title = post.acf.song_name
+            const artist = post.acf.artist_name
+            return (
+                <div className={`${isSmall ? 'less-' : ''}featured-track-wrapper`}>
+                    <div className='feature-track'>
+                        <img src={image} />
+                        {!isSmall && <span className="song-of-day-tag">Song of the day</span>}
+                        <div className="post-info">
+                            <div className="post-square-wrapper date">
+                                <div className='post-date'>
+                                    <p className="month">{month}</p>
+                                    <p className="day">{date}</p>
+                                </div>
+                            </div>
+                            <div className="song-info">
+                                <p className="song-title">{title}</p>
+                                <p className="song-artist">{artist}</p>
+                            </div>
+                            <div className='post-square-wrapper play'>
+                                <img src="http://www.rockwiththis.com/wp-content/uploads/2018/04/unnamed.png" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+        const { posts } = this.props
+        const featuredPostArg = posts[0] || placeholderSong
+        const featuredPost = trackDisplay(featuredPostArg, false)
+
+        const otherPosts = this.props.posts.slice(1).map(post => trackDisplay(post, true))
+
+
 
         return (
             <div>
                 <div className='hero-posts'>
-                    <div className='featured-track'>
-                        <div className='feature-post large'>
-                            <img src={featuredImage} />
-                            <span className="song-of-day-tag">Song of the day</span>
-                            <div className="post-info">
-                                <div className="post-date">
-                                    <span className="month">{featuredMonth}</span><br />
-                                    <span className="day">{featuredDate}</span>
-                                </div>
-                                <p className="song-info">
-                                    <span className="song-title">{featuredTitle}</span><br/>
-                                    <span className="song-artist">{featuredArtist}</span>
-                                </p>
-                                <img className="play-button" src="http://www.rockwiththis.com/wp-content/uploads/2018/04/unnamed.png" />
-                            </div>
-                        </div>
-                    </div>
+                    {featuredPost}
                     <div className='previous-week'>
-                      <div className='feature-post small'>
-                        <img src="http://www.rockwiththis.com/wp-content/uploads/2018/01/Daydream.png" />
-                      </div>
-                      <div className='feature-post small'>
-                        <img src="http://www.rockwiththis.com/wp-content/uploads/2018/01/Daydream.png" />
-                      </div>
-                      <div className='feature-post small'>
-                        <img src="http://www.rockwiththis.com/wp-content/uploads/2018/01/Daydream.png" />
-                      </div>
-                      <div className='feature-post small'>
-                        <img src="http://www.rockwiththis.com/wp-content/uploads/2018/01/Daydream.png" />
-                      </div>
-                      <div className='feature-post small'>
-                        <img src="http://www.rockwiththis.com/wp-content/uploads/2018/01/Daydream.png" />
-                      </div>
-                      <div className='feature-post small'>
-                        <img src="http://www.rockwiththis.com/wp-content/uploads/2018/01/Daydream.png" />
-                      </div>
+                        {otherPosts}
                     </div>
                 </div>
             </div>
