cecc1e949e19f23be2fdbedc5511d2c2a347b83b
Dane Iliff <dane@MacBook-Pro-2.local>
2018-09-29 16:40:02 -0600

    Discover full song tablet styles

--- src/SubmitSongPage.js
+++ src/SubmitSongPage.js
@@ -9,25 +9,8 @@ import { fetchFilters } from './actions/filters'
 import { Helmet } from "react-helmet";
 import Mailchimp from 'react-mailchimp-form'
 import { Link } from 'react-router-dom'
-import introImage from './images/intro-bigger.png'
-import diag2 from './images/diagblue.png'
-import diagcover from './images/diagcoversharp.png'
-import logo1 from './images/connect-logo.svg'
-import logo2 from './images/connect-logo.svg'
-import gridPic from './images/collage-full.png'
-import line from './images/gradient-line.png'
-import tickets from './images/tickets.svg'
-import songs from './images/songs.svg'
-import pic1 from './images/pic1.png'
-import pic2 from './images/pic2.png'
-import pic3 from './images/pic3.png'
-import colorFull from './images/connectColorsFull.png'
-import colorTop from './images/connectColorsTop.png'
-import colorBottom from './images/connectColorsBottom.png'
-import colorIntroLong from './images/connectColorsIntroLong.png'
-import cross from './images/cross.svg'
-import logoWriting from './images/logo-writing-black.png'
 import hands1 from './images/hands.svg'
+
 
 
 
@@ -50,10 +33,9 @@ class SubmitSongPage extends Component {
           </Helmet>
           <div className="submitSongPage page">
             <div className="left-content">
-            <h3>SHOW US WHAT YOU GOT.</h3>
-            <p>Want your song shared with tens of thousands of people all over the world?</p>
-            <p>Send us your email and a link for us to check it out.</p>
-            <p>If we dig it, we’ll post it on our social channels, website, and on our email newsletter. If we don't, we'll still give you some feedback.</p>
+            <h3>SHOW US <br />WHAT YOU GOT.</h3>
+            <hr />
+            <p>Want your song shared with tens of thousands of people all over the world?  Send us the song info, your email and a link for us to check it out. If we dig it, we’ll post the song and a detailed writeup on our social channels, website, and on our email newsletter.  If we don't, we'll still give you some feedback. It could be the best $5 {"you've"} ever spent. </p>
             </div>
 
             <img className="hands1" src={hands1} />
